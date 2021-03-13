cdb0e9e7a44fe95fd0a5a385f6a1717bbc9b2067
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-03-12 21:17:49 -0500

    use <script type="module"> throughout

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,10 +1,6 @@
-import * as accounts from './accounts'
-import * as naj from 'near-api-js'
+import { onChange } from './accounts'
 import render from './render'
 
-// can't import libraries in inline scripts, so let's add them all to window
-window.accounts = accounts
-window.naj = naj
-window.render = render
+onChange(render)
 
-accounts.onChange(render)
+render()
--- src/js/window.d.ts
+++ src/js/window.d.ts
@@ -1,12 +1,5 @@
-import * as accounts from './accounts'
-import * as naj from 'near-api-js'
-import render from './render'
-
 declare global {
   interface Window {
-    accounts: accounts;
-    naj: naj;
-    render: render;
     renderers: (() => {})[]
     state: { [key: string]: any }
   }
