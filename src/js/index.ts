92b3bad82f0fe8910bd0b82121637ab3d89d3fde
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-03-14 21:12:23 -0400

    Revert "use <script type="module"> throughout"
    This reverts commit cdb0e9e7a44fe95fd0a5a385f6a1717bbc9b2067.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,10 @@
-import { onChange } from './accounts'
+import * as accounts from './accounts'
+import * as naj from 'near-api-js'
 import render from './render'
 
-onChange(render)
+// can't import libraries in inline scripts, so let's add them all to window
+window.accounts = accounts
+window.naj = naj
+window.render = render
 
-render()
+accounts.onChange(render)
