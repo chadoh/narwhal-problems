d06964ead72d1ba394096dd9e6d53419ea672abd
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-09-06 17:06:31 -0400

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://chadoh.com",
+  base: "narwhal-problems",
   vite: {
     define: {
-      global: 'window'
-    }
-  }
+      global: "window",
+    },
+  },
 });
